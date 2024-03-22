0496b8047a6643d41f22c536bceef939bc3e2fa4
sul <su1im69n@gmail.com>
2024-03-23 01:41:17 +0300

    hide all it term mode

--- lua/sul/windows.lua
+++ lua/sul/windows.lua
@@ -2,10 +2,27 @@ vim.keymap.set("t", "<C-z>", function()
     vim.cmd.buffer('#')
 end);
 
+local function hideAll()
+    vim.o.showmode = false
+    vim.o.ruler = false
+    vim.o.laststatus = 0
+    vim.o.showcmd = false
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+end
+
+local function showAll()
+    vim.o.showmode = true
+    vim.o.ruler = true
+    vim.o.laststatus = 2
+    vim.o.showcmd = true
+end
+
 local function jump2term()
     if vim.fn.bufname("term://") == "" then
         return vim.cmd.terminal(
-            'powershell.exe -NoExit -Command "&{Import-Module """C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell d6caeb67 -SkipAutomaticLocation -DevCmdArguments """-arch=x64 -host_arch=x64"""}"')
+        -- 'powershell.exe -NoExit -Command "&{Import-Module """C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll"""; Enter-VsDevShell d6caeb67 -SkipAutomaticLocation -DevCmdArguments """-arch=x64 -host_arch=x64"""}"'
+        )
     else
         vim.cmd.buffer("term://")
     end
@@ -13,8 +30,10 @@ end
 
 local function buf2term()
     if '' ~= vim.fn.getbufvar("%", "terminal_job_pid") then
-        return vim.cmd('b#')
+        showAll()
+        return vim.cmd.buffer('#')
     else
+        hideAll()
         jump2term()
     end
 end
