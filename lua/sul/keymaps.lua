0de52a3bd098a69cc0ca60731eb64360b48e6031
sul <su1im69n@gmail.com>
2024-03-23 01:45:20 +0300

    change keys

--- lua/sul/keymaps.lua
+++ lua/sul/keymaps.lua
@@ -3,6 +3,6 @@ vim.g.mapleader = " ";
 vim.keymap.set('i', '<A-Backspace>', '<C-w>');
 vim.keymap.set('i', '<C-q>', '<esc>_<C-q>');
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
-vim.keymap.set("t", "<C-z>", "<C-\\><C-N>");
+vim.keymap.set("t", "<C-t>", "<C-\\><C-N>");
 
 vim.keymap.set({ 'v', 'n' }, "Y", '"+yy');
