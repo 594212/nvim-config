265069e88540a729f20bc98d4f37d70963656bac
sul <su1im69n@gmail.com>
2024-04-03 05:32:21 +0300

    lsp gr move key

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+require("telescope").load_extension('harpoon')
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>hm", '<cmd>Telescope harpoon marks<CR>')
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,6 +5,7 @@ vim.keymap.set('n', '<leader>/', builtin.git_files, {})
 vim.keymap.set('n', '<leader>e', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<C-l>', builtin.lsp_document_symbols);
+vim.keymap.set('n', 'gr', builtin.lsp_references);
 
 vim.keymap.set('n', '<leader>df',
     function()
