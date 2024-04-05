0d51004cb2554eec15c1e4b2cfe308e1041c31e2
sul <Su1im69n@gmail.com>
2024-04-05 14:25:43 +0300

    sioyek for vimtex

--- lua/sul/init.lua
+++ lua/sul/init.lua
@@ -3,6 +3,8 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
+vim.g.vimtex_view_general_viewer = 'sioyek'
+
 
 require("sul.keymaps")
 require("sul.windows")
