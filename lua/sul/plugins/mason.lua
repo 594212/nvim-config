64c60fd49fa0e146051f8de2d51f3dc78925b0c0
sul <Su1im69n@gmail.com>
2024-04-07 21:48:46 +0300

    tex set ru spell checker

--- lua/sul/plugins/mason.lua
+++ lua/sul/plugins/mason.lua
@@ -3,8 +3,4 @@ return {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
     "stevearc/conform.nvim",
-    {
-        'creativenull/efmls-configs-nvim',
-        dependencies = { 'neovim/nvim-lspconfig' },
-    }
 }
