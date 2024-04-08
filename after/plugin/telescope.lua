d338781e4756dc52b428257448341985c5799c6f
sul <Su1im69n@gmail.com>
2024-04-08 08:38:24 +0300

    add workspace search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
+local theme = require('telescope.themes');
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>/', builtin.git_files, {})
@@ -10,7 +11,15 @@ vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols);
 
 vim.keymap.set('n', '<leader>df',
     function()
-        builtin.lsp_document_symbols {
+        builtin.lsp_document_symbols({
+            symbols = { "function", "method" }
+        })
+    end
+);
+
+vim.keymap.set('n', '<leader>wf',
+    function()
+        builtin.lsp_workspace_symbols {
             symbols = { "function", "method" }
         }
     end
@@ -26,3 +35,8 @@ local function find_language_file(lang)
 end
 
 vim.keymap.set('n', '<leader>lp', function() find_language_file('php') end);
+vim.keymap.set('n', '<leader>fe', function()
+    builtin.find_files(theme.get_ivy {
+        cwd = vim.fn.expand("%:p:h")
+    })
+end)
