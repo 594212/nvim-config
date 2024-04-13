e09294a4166003c6d0b199b194463b57928126a6
sul <Su1im69n@gmail.com>
2024-04-13 16:23:28 +0300

    snatch from Chris luasnip

--- lua/sul/plugins/cmp.lua
+++ lua/sul/plugins/cmp.lua
@@ -1,12 +1,22 @@
 return {
-	'hrsh7th/nvim-cmp',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-		'hrsh7th/nvim-cmp',
-		'L3MON4D3/LuaSnip',
-		'saadparwaiz1/cmp_luasnip',
-	}
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/nvim-cmp',
+        'saadparwaiz1/cmp_luasnip',
+        {
+            "saadparwaiz1/cmp_luasnip",
+            event = "InsertEnter",
+        },
+        {
+            "L3MON4D3/LuaSnip",
+            event = "InsertEnter",
+            dependencies = {
+                "rafamadriz/friendly-snippets",
+            },
+        }
+    }
 }
